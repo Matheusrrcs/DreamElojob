75587d02d4f33e1273e84b244c3a1992e0e56d4a
Matheus Rosario <79647787+Matheusrrcs@users.noreply.github.com>
2022-06-09 20:26:51 -0300

    imagem reovlido

--- js/boost-md10.js
+++ js/boost-md10.js
@@ -2,45 +2,41 @@
 //quando mudar a liga atual
 $("#ligaAtual").change(() => {
 
-    if(trataNome("choice") == "valorant"){
-     document.querySelectorAll('#imgEloAtual').forEach(function (item) {
-       item.src = `../imagens/elos/valorant/${ligaAtual.value}.webp`;
-     })
-    }
- 
- else{
-     document.querySelectorAll('#imgEloAtual').forEach(function (item) {
-       item.src = `../imagens/elos/${ligaAtual.value}.png`;
-     })
-   }
-   
-     AddLoad()
-   
-   
-   })
- 
- // liga e desliga a animação de load
-   function AddLoad() {
-     var load = document.getElementById("load")
-   
-     load.classList.remove("d-none")
-   
-     document.getElementById("contentEloFinal").classList.add("d-none")
- 
-   
-     function Load() {
-       load.classList.add("d-none")
-       document.getElementById("contentEloFinal").classList.remove("d-none")
-       
-    
-     }
-   
-     setTimeout(Load, 200);
-   }
- 
- 
- 
-   // trata o title da pagina capturado
- function trataNome(id) {
-   return document.getElementById(id).textContent.toLocaleLowerCase().replaceAll(" ", "").trim()
- }
+
+
+
+  document.querySelectorAll('#imgAtual').forEach(function (item) {
+    item.src = `../imagens/elos/${ligaAtual.value}.png`;
+  })
+
+
+  AddLoad()
+
+
+})
+
+// liga e desliga a animação de load
+function AddLoad() {
+  var load = document.getElementById("load")
+
+  load.classList.remove("d-none")
+
+  document.getElementById("contentEloFinal").classList.add("d-none")
+
+
+  function Load() {
+    load.classList.add("d-none")
+    document.getElementById("contentEloFinal").classList.remove("d-none")
+
+
+  }
+
+  setTimeout(Load, 200);
+}
+
+
+
+// trata o title da pagina capturado
+function trataNome(id) {
+  return document.getElementById(id).textContent.toLocaleLowerCase().replaceAll(" ", "").trim()
+}

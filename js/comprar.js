09971eda7638ddac5489e4aa9ea564417359c4c8
Matheusrrcs <matheus-santana-msn@hotmail.com>
2022-09-22 19:18:30 -0300

    mudado link do whats

--- js/comprar.js
+++ js/comprar.js
@@ -109,7 +109,7 @@ function eloboostDuo() {
 
     }
 
-    var url = `https://wa.me/5571987262223?text=`.concat(text);
+    var url = `https://${type}.whatsapp.com/send?1=pt_BR&phone=5571987262223&text=`.concat(text);
 
 
 
@@ -155,7 +155,7 @@ function md10() {
 
     text = 'Olá,%20estou%20no%20site%20e%20quero%20' + titles + '%20de ' + ch + "%20 " + value + '%20. O%20meu%20elo%20era%20 ' + lga
 
-    var url = `https://wa.me/5571987262223?text=`.concat(text);
+    var url = `https://${type}.whatsapp.com/send?1=pt_BR&phone=5571987262223&text=`.concat(text);
 
 
     window.open(url);
@@ -201,7 +201,7 @@ function coach() {
         text = 'Olá,%20estou%20no%20site%20e%20quero%20' + titles + '%20de ' + ch + "%20de " + lga + " partidas%20" + value
 
     }
-    var url = `https://wa.me/5571987262223?text=`.concat(text);
+    var url = `https://${type}.whatsapp.com/send?1=pt_BR&phone=5571987262223&text=`.concat(text);
 
 
     window.open(url);
